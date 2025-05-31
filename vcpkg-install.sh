58c7eaa913fbcaa54f45a3e5145eee85dc3f229e
sv-sand <sve.snd@gmail.com>
2025-05-31 11:30:37 +0300

    issue-54: so compiled and added

--- vcpkg-install.sh
+++ vcpkg-install.sh
@@ -1,4 +1,4 @@
 # This script installs the required dependencies
 # Before executing, install and add vcpkg to system PATH
 
-vcpkg install --triplet x64-windows-static-md
+vcpkg install --triplet x64-linux
